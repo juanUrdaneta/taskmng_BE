d2c31853b0bed614b4b94446dd0e3840a2cc7e46
juanUrdaneta <juankorus@gmail.com>
2020-11-11 20:56:31 -0300

    refactoring

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,8 +3,8 @@ const Task = require('../models/TaskModel');
 // const catchAsync = require('../utils/catchAsyncMethod');
 const Factory = require('./handlerFactory');
 
-exports.createTask = Factory.createOne(Task);
-exports.getAllTasks = Factory.getAll(Task);
 exports.getTask = Factory.getOne(Task);
+exports.getAllTasks = Factory.getAll(Task);
+exports.createTask = Factory.createOne(Task);
 exports.updateTask = Factory.updateOne(Task);
 exports.deleteTask = Factory.deleteOne(Task);
