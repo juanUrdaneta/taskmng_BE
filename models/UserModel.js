5bac06b827f55c7d7db62a30c2b45e342d5a4b8e
juanUrdaneta <juankorus@gmail.com>
2020-11-12 01:11:26 -0300

    authController added

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,35 +1,45 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: [true, 'A user must have a name'],
-        minlength: [4, 'A user name cannot contain less than 4 characters'],
-        maxlength: [30, 'A user name cannot contain more than 30 characters'],
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: [true, 'A user must have an email'],
-    },
-    password: {
-        type: String,
-        required: [true, 'A user must have a password'],
-        select: false,
-    },
-    role: {
-        type: String,
-        required: [true, 'A user must have a role assigned'],
-        enum: {
-            values: ['admin', 'user'],
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: [true, 'A user must have a name'],
+            minlength: [4, 'A user name cannot contain less than 4 characters'],
+            maxlength: [
+                30,
+                'A user name cannot contain more than 30 characters',
+            ],
         },
-        default: 'user',
+        email: {
+            type: String,
+            unique: true,
+            required: [true, 'A user must have an email'],
+        },
+        password: {
+            type: String,
+            required: [true, 'A user must have a password'],
+            select: false,
+        },
+        role: {
+            type: String,
+            required: [true, 'A user must have a role assigned'],
+            enum: {
+                values: ['admin', 'user'],
+            },
+            default: 'user',
+        },
+        acronim: String,
     },
-    acronim: String,
-});
+    {
+        toObject: { virtuals: true },
+        toJSON: { virtuals: true },
+    }
+);
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', async function (next) {
     const name = this.name.split(' ');
     const [firstName, lastname] = [name[0], name[1]];
 
@@ -37,8 +47,18 @@ userSchema.pre('save', function (next) {
         .toUpperCase()
         .concat(lastname[0].toUpperCase() || '');
 
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+
     next();
 });
+
+userSchema.methods.confirmPassword = async function (
+    candidatePassword,
+    userPassword
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const taskController = require('../controllers/taskController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
