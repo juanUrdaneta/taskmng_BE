9e7683a4c2da5fe7cb038a956c594d3fb6bcedab
juanUrdaneta <juankorus@gmail.com>
2020-11-12 01:10:55 -0300

    authentication module added

---
+++ controllers/authController.js
@@ -0,0 +1,90 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+
+const catchAsync = require('../utils/catchAsyncMethod');
+const AppError = require('../utils/appErrors');
+const User = require('../models/UserModel');
+
+const signToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
+
+const cookieOptions = {
+    expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httOnly: true,
+};
+
+const createAndSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+    user.password = undefined;
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    res.cookie('jwt', token, cookieOptions);
+    res.status(statusCode).json({
+        statis: 'success',
+        data: user,
+        token: token,
+    });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        role: 'user',
+    });
+
+    createAndSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.confirmPassword(password, user.password))) {
+        return next(new AppError('Invalid credentials', 400));
+    }
+
+    createAndSendToken(user, 200, res);
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+        return next(new AppError('You are not logged in! Please log in', 401));
+    }
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser)
+        return next(new AppError('The user no longer exist', 401));
+
+    req.user = currentUser;
+    next();
+});
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new AppError(
+                    "You don't have permission to perform this action",
+                    403
+                )
+            );
+        }
+        next();
+    };
+};
