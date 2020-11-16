73300365d96ff6551c8891438ca8fbe1972fffa1
juanUrdaneta <juankorus@gmail.com>
2020-11-16 10:07:22 -0300

    corrected typo

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ const createAndSendToken = (user, statusCode, res) => {
     if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
     res.cookie('jwt', token, cookieOptions);
     res.status(statusCode).json({
-        statis: 'success',
+        status: 'success',
         data: user,
         token: token,
     });
